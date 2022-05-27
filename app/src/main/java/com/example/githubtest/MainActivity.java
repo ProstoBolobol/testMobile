771030e00e9b73ce4afeef1a593758b2353315b9
ProstoBolobol <gagags2017@gmail.com>
2022-05-27 15:41:04 +0300

    Добавил комментарий

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+// Привет АНдрйе, ту ру ру ру ру.
 public class MainActivity extends AppCompatActivity {
 
     @Override
